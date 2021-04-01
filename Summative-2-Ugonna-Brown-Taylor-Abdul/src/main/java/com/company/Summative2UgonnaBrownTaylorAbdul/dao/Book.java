22685d4b942e23bc3ee768117256569b21238442
Zaid <z.i.abdul-kaudeyr@spartans.nsu.edu>
2021-04-01 19:43:05 -0400

    book dao

--- Summative-2-Ugonna-Brown-Taylor-Abdul/src/main/java/com/company/Summative2UgonnaBrownTaylorAbdul/dao/Book.java
+++ Summative-2-Ugonna-Brown-Taylor-Abdul/src/main/java/com/company/Summative2UgonnaBrownTaylorAbdul/dao/Book.java
@@ -1,4 +1,91 @@
 package com.company.Summative2UgonnaBrownTaylorAbdul.dao;
 
-public class Book {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Book
+{
+
+    //properties
+    private int bookID;
+    private String isbn;
+    private LocalDate publishDate;
+    private int authorID;
+    private String title;
+    private int publisherID;
+    private BigDecimal price;
+
+    //getters and setters
+
+    public int getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public LocalDate getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(LocalDate publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public int getAuthorID() {
+        return authorID;
+    }
+
+    public void setAuthorID(int authorID) {
+        this.authorID = authorID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPublisherID() {
+        return publisherID;
+    }
+
+    public void setPublisherID(int publisherID) {
+        this.publisherID = publisherID;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    //Equals and HashCode
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return bookID == book.bookID && authorID == book.authorID && publisherID == book.publisherID && Objects.equals(isbn, book.isbn) && Objects.equals(publishDate, book.publishDate) && Objects.equals(title, book.title) && Objects.equals(price, book.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookID, isbn, publishDate, authorID, title, publisherID, price);
+    }
 }
