6725727e5b29c2d6e00a1e139a5fe4a6ccf7b907
aniya <aniyanb00@gmail.com>
2021-04-01 19:33:52 -0400

    author model added

--- Summative-2-Ugonna-Brown-Taylor-Abdul/src/main/java/com/company/Summative2UgonnaBrownTaylorAbdul/dao/Author.java
+++ Summative-2-Ugonna-Brown-Taylor-Abdul/src/main/java/com/company/Summative2UgonnaBrownTaylorAbdul/dao/Author.java
@@ -1,4 +1,103 @@
 package com.company.Summative2UgonnaBrownTaylorAbdul.dao;
 
+import java.util.Objects;
+
 public class Author {
+
+    private int authorID;
+    private String firstName;
+    private String lastName;
+    private String street;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String phone;
+    private String email;
+
+    //getters and setters
+
+    public int getAuthorID() {
+        return authorID;
+    }
+
+    public void setAuthorID(int authorID) {
+        this.authorID = authorID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return getAuthorID() == author.getAuthorID() && Objects.equals(getFirstName(), author.getFirstName()) && Objects.equals(getLastName(), author.getLastName()) && Objects.equals(getStreet(), author.getStreet()) && Objects.equals(getCity(), author.getCity()) && Objects.equals(getState(), author.getState()) && Objects.equals(getPostalCode(), author.getPostalCode()) && Objects.equals(getPhone(), author.getPhone()) && Objects.equals(getEmail(), author.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getAuthorID(), getFirstName(), getLastName(), getStreet(), getCity(), getState(), getPostalCode(), getPhone(), getEmail());
+    }
 }
