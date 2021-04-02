902488560e90b4824637afd6df880adda5cddd3a
Chloe J. Taylor <44882011+steakbaby2@users.noreply.github.com>
2021-04-01 20:31:41 -0400

    Added Interfaces and stuff

--- Summative-2-Ugonna-Brown-Taylor-Abdul/src/main/java/com/company/Summative2UgonnaBrownTaylorAbdul/dao/PublisherDayJdbcTemplateImpl.java
+++ Summative-2-Ugonna-Brown-Taylor-Abdul/src/main/java/com/company/Summative2UgonnaBrownTaylorAbdul/dao/PublisherDayJdbcTemplateImpl.java
@@ -1,4 +1,38 @@
 package com.company.Summative2UgonnaBrownTaylorAbdul.dao;
 
+import com.company.Summative2UgonnaBrownTaylorAbdul.model.Publisher;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class PublisherDayJdbcTemplateImpl implements PublisherDao{
+
+    //Prepared statements strings
+
+
+    @Override
+    public Publisher addPublisher(Publisher publisher){
+        return null;
+    }
+
+    @Override
+    public Publisher getPublisher(int id){
+        return null;
+    }
+
+    @Override
+    public List<Publisher> getAllPublishers(){
+        return null;
+    }
+
+    @Override
+    public void updatePublisher(Publisher publisher){
+
+    }
+
+    @Override
+    public void deletePublisher(int id){
+
+    }
 }
