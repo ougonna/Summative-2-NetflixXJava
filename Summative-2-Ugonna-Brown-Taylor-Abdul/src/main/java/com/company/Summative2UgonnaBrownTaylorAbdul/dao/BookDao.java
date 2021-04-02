3d761388450228f8301e0513b2b955a0a0f1fc01
Zaid <z.i.abdul-kaudeyr@spartans.nsu.edu>
2021-04-01 20:31:41 -0400

    book dao

--- Summative-2-Ugonna-Brown-Taylor-Abdul/src/main/java/com/company/Summative2UgonnaBrownTaylorAbdul/dao/BookDao.java
+++ Summative-2-Ugonna-Brown-Taylor-Abdul/src/main/java/com/company/Summative2UgonnaBrownTaylorAbdul/dao/BookDao.java
@@ -1,4 +1,20 @@
 package com.company.Summative2UgonnaBrownTaylorAbdul.dao;
 
-public interface BookDao {
+import com.company.Summative2UgonnaBrownTaylorAbdul.model.Book;
+
+import java.util.List;
+
+public interface BookDao
+{
+    Book getBook(int bookID);
+
+    List<Book> getAllBooks();
+
+    Book addBook(Book book);
+
+    void updateBook(Book book);
+
+    void deleteBook(int bookID);
+
+    List <Book> getBooksByAuthor(int authorID);
 }
