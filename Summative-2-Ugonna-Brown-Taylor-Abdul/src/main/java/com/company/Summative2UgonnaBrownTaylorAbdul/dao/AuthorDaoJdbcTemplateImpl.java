6cd698a2106da491f9d5fef238e68bd127b6d637
aniya <aniyanb00@gmail.com>
2021-04-05 17:34:56 -0400

    added code to the implementation file for

--- Summative-2-Ugonna-Brown-Taylor-Abdul/src/main/java/com/company/Summative2UgonnaBrownTaylorAbdul/dao/AuthorDaoJdbcTemplateImpl.java
+++ Summative-2-Ugonna-Brown-Taylor-Abdul/src/main/java/com/company/Summative2UgonnaBrownTaylorAbdul/dao/AuthorDaoJdbcTemplateImpl.java
@@ -2,9 +2,13 @@ package com.company.Summative2UgonnaBrownTaylorAbdul.dao;
 
 import com.company.Summative2UgonnaBrownTaylorAbdul.model.Author;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
@@ -12,7 +16,8 @@ public class AuthorDaoJdbcTemplateImpl implements AuthorDao{
 
     // Prepared statement strings
     private static final String INSERT_AUTHOR_SQL =
-            "insert into author (first_name, last_name, street, city, state, postal_code,phone,email) values (?, ?, ?, ?, ?, ?, ? ,?)";
+            "insert into author (first_name, last_name, street, city, state, postal_code,phone,email) " +
+                    "values (?, ?, ?, ?, ?, ?, ? ,?)";
 
     private static final String SELECT_AUTHOR_SQL =
             "select * from author where id = ?";
@@ -24,7 +29,8 @@ public class AuthorDaoJdbcTemplateImpl implements AuthorDao{
             "delete from author where id = ?";
 
     private static final String UPDATE_AUTHOR_SQL =
-            "update car set make = ?, model = ?, year = ?, color = ? where id = ?";
+            "update author set first_name = ?, last_name = ?, street = ?, city = ?, state = ?, " +
+                    "postal_code = ?, phone = ?, email = ?" ;
 
 
     private final JdbcTemplate jdbcTemplate;
@@ -35,23 +41,77 @@ public class AuthorDaoJdbcTemplateImpl implements AuthorDao{
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    @Override
+    @Transactional
     public Author addAuthor(Author author){
+
+        jdbcTemplate.update(INSERT_AUTHOR_SQL,
+                author.getFirstName(),
+                author.getLastName(),
+                author.getStreet(),
+                author.getCity(),
+                author.getState(),
+                author.getPostalCode(),
+                author.getPhone(),
+                author.getEmail());
+
+        int id = jdbcTemplate.queryForObject("select last_insert_id()", Integer.class);
+
+        author.setAuthorID(id);
+
         return author;
     }
 
+    @Override
     public Author getAuthor (int id){
-        return null;
+        try{
+            return jdbcTemplate.queryForObject(SELECT_AUTHOR_SQL,this::mapRowToCar,id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
+    @Override
     public List<Author> getAllAuthors(){
-        return null;
+
+        return jdbcTemplate.query(SELECT_ALL_AUTHOR_SQL, this::mapRowToCar);
     }
 
+    @Override
     public void updateAuthor(Author author){
 
+        jdbcTemplate.update(UPDATE_AUTHOR_SQL,
+                author.getFirstName(),
+                author.getLastName(),
+                author.getStreet(),
+                author.getCity(),
+                author.getState(),
+                author.getPostalCode(),
+                author.getPhone(),
+                author.getEmail());
+
     }
 
+    @Override
     public void deleteAuthor(int id){
+        jdbcTemplate.update(DELETE_AUTHOR_SQL,id);
 
+    }
+
+
+    private Author mapRowToCar(ResultSet rs, int rowNum) throws SQLException {
+        Author author = new Author();
+        author.setAuthorID(rs.getInt("author_id"));
+        author.setFirstName(rs.getString("first_name"));
+        author.setLastName(rs.getString("last_name"));
+        author.setStreet(rs.getString("street"));
+        author.setCity(rs.getString("city"));
+        author.setState(rs.getString("state"));
+        author.setPostalCode(rs.getString("postal_code"));
+        author.setPhone(rs.getString("phone"));
+        author.setEmail(rs.getString("email"));
+
+        return author;
     }
 }
