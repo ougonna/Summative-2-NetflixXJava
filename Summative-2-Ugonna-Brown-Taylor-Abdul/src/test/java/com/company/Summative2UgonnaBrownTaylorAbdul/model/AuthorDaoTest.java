dd5faf19704d558d10b07ec7708b0e5a34f73436
Ogechi Ugonna <ogeugonna@gmail.com>
2021-04-05 17:33:14 -0400

    Completed Test for all three entities, Changed variable for getBooksByAuthor to String

--- Summative-2-Ugonna-Brown-Taylor-Abdul/src/test/java/com/company/Summative2UgonnaBrownTaylorAbdul/model/AuthorDaoTest.java
+++ Summative-2-Ugonna-Brown-Taylor-Abdul/src/test/java/com/company/Summative2UgonnaBrownTaylorAbdul/model/AuthorDaoTest.java
@@ -1,6 +1,8 @@
 package com.company.Summative2UgonnaBrownTaylorAbdul.model;
 
 import com.company.Summative2UgonnaBrownTaylorAbdul.dao.AuthorDao;
+import com.company.Summative2UgonnaBrownTaylorAbdul.dao.BookDao;
+import com.company.Summative2UgonnaBrownTaylorAbdul.dao.PublisherDao;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,18 +18,35 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class AuthorDaoTest {
+
     @Autowired
-    protected AuthorDao dao;
+    AuthorDao authorDao;
+    @Autowired
+    BookDao bookDao;
+    @Autowired
+    PublisherDao publisherDao;
+
 
     @Before
-    public void setUp() throws Exception{
+    public void setUp() throws Exception {
 
-        //clean out database
-        List<Author> aList = dao.getAllAuthors();
-
-        for (Author a : aList){
-            dao.deleteAuthor(a.getAuthorID());
+        //clean out the db
+        List<Author> aList = authorDao.getAllAuthors();
+        for (Author a : aList) {
+            authorDao.deleteAuthor(a.getAuthorID());
         }
+
+        List<Publisher> pList = publisherDao.getAllPublishers();
+        for (Publisher p : pList) {
+            publisherDao.deletePublisher(p.getId());
+        }
+
+        List<Book> dList = bookDao.getAllBooks();
+
+        for (Book d : dList) {
+            bookDao.deleteBook(d.getBookID());
+        }
+
     }
 
     @Test
@@ -45,20 +64,97 @@ public class AuthorDaoTest {
         auth.setPostalCode("34509");
 
         //act
-        auth = dao.addAuthor(auth);
-        Author auth2 = dao.getAuthor(auth.getAuthorID());
+        auth = authorDao.addAuthor(auth);
+        Author auth2 = authorDao.getAuthor(auth.getAuthorID());
 
         //Assert
         assertEquals(auth, auth2);
 
         //Act
-        dao.deleteAuthor(auth.getAuthorID());
-        auth2 = dao.getAuthor(auth.getAuthorID());
+        authorDao.deleteAuthor(auth.getAuthorID());
+        auth2 = authorDao.getAuthor(auth.getAuthorID());
 
         //Assert
         assertNull(auth2);
 
     }
+
+    @Test
+    public void getALlAuthors(){
+
+        //Arrange: arranging first Authors
+        Author auth = new Author();
+        auth.setFirstName("BookSecond");
+        auth.setLastName("BookSecondLast");
+        auth.setEmail("seclast@gmail.com");
+        auth.setPhone("2224445678");
+        auth.setCity("Norfolk");
+        auth.setState("PA");
+        auth.setStreet("700 park road");
+        auth.setPostalCode("34456");
+
+        //Act
+        authorDao.addAuthor(auth);
+
+        //Arrange: Arrange second motorcycle
+        auth = new Author();
+        auth.setFirstName("BookFirstLast");
+        auth.setLastName("BookLastLast");
+        auth.setEmail("lastlast@gmail.com");
+        auth.setPhone("3335555678");
+        auth.setCity("Washington");
+        auth.setState("DC");
+        auth.setStreet("700 road ave");
+        auth.setPostalCode("34510");
+
+        //Act
+        authorDao.addAuthor(auth);
+        List<Author> authorList = authorDao.getAllAuthors(); //gets all authors
+
+        //Assert
+        assertEquals(authorList.size(), 2);
+
+    }
+
+
+
+    @Test
+    public void updateAuthors(){
+
+        //Arrange
+        Author auth = new Author();
+        auth.setFirstName("UpdateName");
+        auth.setLastName("UpdateLast");
+        auth.setEmail("Updatelast@gmail.com");
+        auth.setPhone("99944455467");
+        auth.setCity("Manassas");
+        auth.setState("VA");
+        auth.setStreet("Update 700 park ave");
+        auth.setPostalCode("33450");
+
+        //Act
+        auth = authorDao.addAuthor(auth);
+
+        //Arrange
+        auth = new Author();
+        auth.setFirstName("Update2Name");
+        auth.setLastName("Update2Last");
+        auth.setEmail("Updatelast2@gmail.com");
+        auth.setPhone("99927855467");
+        auth.setCity("Norfolk");
+        auth.setState("VA");
+        auth.setStreet("Update2 700 park ave");
+        auth.setPostalCode("32150");
+
+        //Act
+        authorDao.updateAuthor(auth); //updates db with new information
+        Author auth2 = authorDao.getAuthor(auth.getAuthorID()); //creates another motorcycle
+                                                          //gets id of the motorcycle created above and assign to auth2
+
+        //Assert
+        assertEquals(auth2, auth);
+    }
+
 
 
 }
--- Summative-2-Ugonna-Brown-Taylor-Abdul/src/test/java/com/company/Summative2UgonnaBrownTaylorAbdul/model/BookDaoTest.java
+++ Summative-2-Ugonna-Brown-Taylor-Abdul/src/test/java/com/company/Summative2UgonnaBrownTaylorAbdul/model/BookDaoTest.java
@@ -1,4 +1,269 @@
 package com.company.Summative2UgonnaBrownTaylorAbdul.model;
 
+import com.company.Summative2UgonnaBrownTaylorAbdul.dao.AuthorDao;
+import com.company.Summative2UgonnaBrownTaylorAbdul.dao.BookDao;
+import com.company.Summative2UgonnaBrownTaylorAbdul.dao.PublisherDao;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.validator.PublicClassValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class BookDaoTest {
+
+    @Autowired
+    AuthorDao authorDao;
+    @Autowired
+    BookDao bookDao;
+    @Autowired
+    PublisherDao publisherDao;
+
+
+    @Before
+    public void setUp() throws Exception{
+
+        //clean out the db
+        List<Author> aList = authorDao.getAllAuthors();
+        for(Author a : aList){
+            authorDao.deleteAuthor(a.getAuthorID());
+        }
+
+        List<Publisher> pList = publisherDao.getAllPublishers();
+        for(Publisher p : pList){
+            publisherDao.deletePublisher(p.getId());
+        }
+
+        List<Book> dList = bookDao.getAllBooks();
+
+        for (Book d : dList){
+            bookDao.deleteBook(d.getBookID());
+        }
+
+    }
+
+    @Test
+    public void addGetDeleteBook(){
+
+        //Arrange
+        Author author = new Author();
+        author.setFirstName("UpdateName");
+        author.setLastName("UpdateLast");
+        author.setEmail("Updatelast@gmail.com");
+        author.setPhone("99944455467");
+        author.setCity("Manassas");
+        author.setState("VA");
+        author.setStreet("Update 700 park ave");
+        author.setPostalCode("33450");
+        author = authorDao.addAuthor(author);
+
+        Publisher publisher = new Publisher();
+        publisher.setName("PublisherTest");
+        publisher.setEmail("publish@gmail.com");
+        publisher.setStreet("Test street");
+        publisher.setPhone("4473347877");
+        publisher.setCity("Norfolk");
+        publisher.setState("VA");
+        publisher.setPostalCode("33456");
+        publisher = publisherDao.addPublisher(publisher);
+
+        Book book = new Book();
+        book.setTitle("testBook");
+        book.setIsbn("TEST6578");
+        book.setPrice(new BigDecimal("21.95"));
+        book.setPublishDate(LocalDate.of(2010, 8, 5));
+        book.setAuthorID(author.getAuthorID());
+        book.setPublisherID(publisher.getId());
+        book = bookDao.addBook(book);
+
+        Book book1 = bookDao.getBook(book.getBookID()); //creates another book
+                                                        //gets id of book just added and assigns to book1
+
+        //Assert
+        assertEquals(book1, book);
+
+        bookDao.deleteBook(book.getBookID()); //deletes book just added
+        book1 = bookDao.getBook(book.getBookID()); //gets empty book which is null and assigns to book1
+
+        //Assert
+        assertNull(book1);
+    }
+
+    @Test(expected = DataIntegrityViolationException.class)  //throws an exception if for any reason the one to one mapping is broken (e.g if author gets deleted without deleting the book, an exception will be thrown)
+    public void addWithRefIntegrityException(){
+
+        Book book = new Book();
+        book.setTitle("testBook");
+        book.setIsbn("TEST6578");
+        book.setPrice(new BigDecimal("21.95"));
+        book.setPublishDate(LocalDate.of(2010, 8, 5));
+        book.setAuthorID(22);
+        book.setPublisherID(34);
+
+        book = bookDao.addBook(book);
+
+    }
+
+    @Test
+    public void getAllBooks() {
+
+        //Need to create an author and publisher first
+        Author author = new Author();
+        author.setFirstName("UpdateName");
+        author.setLastName("UpdateLast");
+        author.setEmail("Updatelast@gmail.com");
+        author.setPhone("99944455467");
+        author.setCity("Manassas");
+        author.setState("VA");
+        author.setStreet("Update 700 park ave");
+        author.setPostalCode("33450");
+        author = authorDao.addAuthor(author);
+
+        Publisher publisher = new Publisher();
+        publisher.setName("PublisherTest");
+        publisher.setEmail("publish@gmail.com");
+        publisher.setStreet("Test street");
+        publisher.setPhone("4473347877");
+        publisher.setCity("Norfolk");
+        publisher.setState("VA");
+        publisher.setPostalCode("33456");
+        publisher = publisherDao.addPublisher(publisher);
+
+        //add first book
+        Book book = new Book();
+        book.setTitle("testBook");
+        book.setIsbn("TEST6578");
+        book.setPrice(new BigDecimal("21.95"));
+        book.setPublishDate(LocalDate.of(2010, 8, 5));
+        book.setAuthorID(author.getAuthorID());
+        book.setPublisherID(publisher.getId());
+        book = bookDao.addBook(book);
+
+        //add second book
+        book = new Book();
+        book.setTitle("testBook20");
+        book.setIsbn("TEST6578-22");
+        book.setPrice(new BigDecimal("30.95"));
+        book.setPublishDate(LocalDate.of(2012, 9, 9));
+        book.setAuthorID(author.getAuthorID());
+        book.setPublisherID(publisher.getId());
+        book = bookDao.addBook(book);
+
+        List<Book> bList = bookDao.getAllBooks();
+
+        assertEquals(bList.size(), 2);
+    }
+
+    @Test
+    public void getBookByAuthor(){
+
+        //Need to create an author and publisher first
+        Author author = new Author();
+        author.setFirstName("UpdateName");
+        author.setLastName("UpdateLast");
+        author.setEmail("Updatelast@gmail.com");
+        author.setPhone("99944455467");
+        author.setCity("Manassas");
+        author.setState("VA");
+        author.setStreet("Update 700 park ave");
+        author.setPostalCode("33450");
+        author = authorDao.addAuthor(author);
+
+        Publisher publisher = new Publisher();
+        publisher.setName("PublisherTest");
+        publisher.setEmail("publish@gmail.com");
+        publisher.setStreet("Test street");
+        publisher.setPhone("4473347877");
+        publisher.setCity("Norfolk");
+        publisher.setState("VA");
+        publisher.setPostalCode("33456");
+        publisher = publisherDao.addPublisher(publisher);
+
+        //add first book
+        Book book = new Book();
+        book.setTitle("testBook");
+        book.setIsbn("TEST6578");
+        book.setPrice(new BigDecimal("21.95"));
+        book.setPublishDate(LocalDate.of(2010, 8, 5));
+        book.setAuthorID(author.getAuthorID());
+        book.setPublisherID(publisher.getId());
+        book = bookDao.addBook(book);
+
+        //add second book
+        book = new Book();
+        book.setTitle("testBook20");
+        book.setIsbn("TEST6578-22");
+        book.setPrice(new BigDecimal("30.95"));
+        book.setPublishDate(LocalDate.of(2012, 9, 9));
+        book.setAuthorID(author.getAuthorID());
+        book.setPublisherID(publisher.getId());
+        book = bookDao.addBook(book);
+
+        //Act
+        bookDao.addBook(book);
+        List<Book> bList = bookDao.getBooksByAuthor("PublisherTest");
+
+        //Assert
+        assertEquals(bList.size(), 2);
+
+    }
+
+    @Test
+    public  void updateBook(){
+
+        //Need to create an author and publisher first
+        Author author = new Author();
+        author.setFirstName("UpdateName");
+        author.setLastName("UpdateLast");
+        author.setEmail("Updatelast@gmail.com");
+        author.setPhone("99944455467");
+        author.setCity("Manassas");
+        author.setState("VA");
+        author.setStreet("Update 700 park ave");
+        author.setPostalCode("33450");
+        author = authorDao.addAuthor(author);
+
+        Publisher publisher = new Publisher();
+        publisher.setName("PublisherTest");
+        publisher.setEmail("publish@gmail.com");
+        publisher.setStreet("Test street");
+        publisher.setPhone("4473347877");
+        publisher.setCity("Norfolk");
+        publisher.setState("VA");
+        publisher.setPostalCode("33456");
+        publisher = publisherDao.addPublisher(publisher);
+
+        //add first book
+        Book book = new Book();
+        book.setTitle("testBook");
+        book.setIsbn("TEST6578");
+        book.setPrice(new BigDecimal("21.95"));
+        book.setPublishDate(LocalDate.of(2010, 8, 5));
+        book.setAuthorID(author.getAuthorID());
+        book.setPublisherID(publisher.getId());
+        book = bookDao.addBook(book);
+
+
+        //new information to be updated
+        book.setTitle("NEW TITLE");
+        book.setPublishDate(LocalDate.of(2020, 12, 6));
+        book.setPrice(new BigDecimal("50.47"));
+
+        bookDao.updateBook(book);  //updates book
+
+        Book book1 = bookDao.getBook(book.getBookID()); //creates new book and assigns updated book to it (book1)
+        assertEquals(book1, book);
+
+    }
 }
